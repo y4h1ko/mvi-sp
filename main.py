80d17000c6c0fb9009cdd8f2c397faa8af8f8ab9
y4h1ko <adam0777777@gmail.com>
2025-11-20 23:03:10 +0100

    has running model wioth head withflow

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from code_file.train_and_test import *
 from code_file.visualizations import *
 
 #main loop
-def main(plot1: bool = False, plot2: bool = False):
+def main1(plot1: bool = False, plot2: bool = False):
     #setup for reproducibility and device
     set_seed()
     device = set_device()
@@ -44,4 +44,43 @@ def main(plot1: bool = False, plot2: bool = False):
 
 
 
-main(plot1=True)
+#main loop
+def main2():
+    #setup for reproducibility and device
+    set_seed()
+    device = set_device()
+
+    #setup for model and more
+    model = TransformerModel2().to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.learning_rate, weight_decay=cfg.weight_decay)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=cfg.epochs)
+
+
+    #creating dataset and converting to tensor dataset
+    V_np, tar_np, t_np = make_sine_dataset(noise=True)
+    ds_full = from_array_to_tensor_dataset(V_np, tar_np)
+
+
+    #splitting to train, val and test parts
+    train_loader, val_loader, test_loader = split_and_load(ds_full)
+
+    #training and validation steps
+    model, train_mse_hist, val_mse_hist = train_and_eval_training_flow(train_loader, val_loader, device, model, optimizer, scheduler)
+
+    #test step
+    test_mse, test_mae = evaluate(test_loader, model, device)
+    print(f"Test MSE {test_mse:.6f}, MAE {test_mae:.6f}")
+
+    #plotting data collection
+    y_true, y_pred = prediction_collecter_plot(test_loader, model, device)
+
+    #plots - save or show option
+    plot_pred_vs_true(y_true, y_pred, test_mse, test_mae, save_plot=True, show_plot=True)
+    plot_loss_curves(train_mse_hist, val_mse_hist, save_plot=True, show_plot=True)
+    plot_loss_curves(train_mse_hist, val_mse_hist, save_plot=True, show_plot=True, y_limit=0.025, zoom='0.025')
+
+
+
+
+#main1()
+main2()
