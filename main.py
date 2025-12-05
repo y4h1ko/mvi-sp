3795e18dcdb1b9094b8b06d9e422efc19c043fc6
y4h1ko <adam0777777@gmail.com>
2025-12-05 21:55:24 +0100

    another modified models and another things

--- main.py
+++ main.py
@@ -88,5 +88,40 @@ def main2():
 
     plot_error_vs_true_omega(y_true, y_pred, save_plot=False, show_plot=True)
 
+
+
+#main loop
+def main3():
+    #setup for reproducibility and device
+    set_seed()
+    device = set_device()
+
+    #setup for model and more
+    model = TransformerModel3(out_dim=2).to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.learning_rate, weight_decay=cfg.weight_decay)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=cfg.epochs)
+
+    #creating dataset and converting to tensor dataset
+    #V_np, tar_np, t_np = make_sine_dataset(noise=True)
+    V_np, tar_np, t_np = make_double_sine_dataset(noise=True)
+    ds_full = from_array_to_tensor_dataset(V_np, tar_np)
+
+    #splitting to train, val and test parts
+    train_loader, val_loader, test_loader = split_and_load(ds_full)
+
+    #training and validation steps
+    model, train_mse_hist, val_mse_hist = train_and_eval_training_flow2(train_loader, val_loader, device, model, optimizer, scheduler)
+
+    #test step
+    test_mse, test_mae = evaluate_hybrid(test_loader, model, device)
+    print(f"Test MSE {test_mse:.6f}, MAE {test_mae:.6f}")
+
+    #plotting data collection
+    y_true, y_pred = prediction_collecter_plot_twofreq_perm(test_loader, model, device)
+
+    #plots - save or show option
+    
+    
 #main1()
 #main2()
+#main3()
