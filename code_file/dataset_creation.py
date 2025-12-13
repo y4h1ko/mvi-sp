3280c383d787e9daa75c86018aa326c9863fc4ad
y4h1ko <adam0777777@gmail.com>
2025-12-13 09:14:29 +0100

    added nonlinear wave dataset

--- code_file/dataset_creation.py
+++ code_file/dataset_creation.py
@@ -88,6 +88,36 @@ def make_double_sine_dataset(N: int=cfg.num_of_samples, t_interval: list=cfg.tim
     #return tuple of arrays of 
     return np.array(V), np.array(target), t_val
 
+def make_double_sine_nonlinear_dataset(N: int=cfg.num_of_samples, t_interval: list=cfg.time_interval, t_k: int=cfg.discr_of_time, w_min: float=cfg.omega_min, 
+                    w_max: float=cfg.omega_max, seed=cfg.seed, mu: float=cfg.mu, sigma: float=cfg.noise_std, noise: bool=False):
+    '''
+    Generate a dataset with N samples of sine waves y=sin(wi*t) + sin(wj*t) + sin(wi*t) * sin(wj*t) with frequencies w sampled uniformly from [w_min, w_max] - both limit values are included.
+    Optionally, Gaussian noise N(mu, sigma) can be added to each sample.
+
+    Returns: tuple of NumPy arrays (V, target, t_val)
+    '''
+
+    rng = np.random.default_rng(seed)
+
+    #evenly space time in t_k levels
+    t_val = np.linspace(t_interval[0], t_interval[1], t_k)
+    #generate N frequencies from [w_min ,w_max] for two mix omegas
+    w_val = rng.uniform(w_min, w_max, size=(N,2))
+    
+    V, target = [], []
+
+    #generate N samples of sin(wi*t) + sin(wj*t)
+    for w1, w2 in w_val:
+        if noise:
+            Vi = 1 * np.sin(w1 * t_val) + 1 * np.sin(w2 * t_val) + np.sin(w1 * t_val) * np.sin(w2 * t_val) + rng.normal(mu, sigma, size=t_k)
+        else:
+            Vi = 1 * np.sin(w1 * t_val) + 1 * np.sin(w2 * t_val) + np.sin(w1 * t_val) * np.sin(w2 * t_val)
+        V.append(np.array(Vi))
+        
+        target.append(np.array([w1,w2]))
+    #return tuple of arrays of 
+    return np.array(V), np.array(target), t_val
+
 #make_sine_dataset(noise=True)
 #make_double_sine_dataset(noise=True)
 
